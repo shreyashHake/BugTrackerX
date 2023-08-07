5f134d190363bf234434a11cde7437ffa8b12e64
Shreyash <81287478+shreyashHake@users.noreply.github.com>
2023-08-07 18:04:55 +0530

    Added exception handing in CustomerControlller

--- backend/src/main/java/com/springBoot/eBugTracker/controller/CustomerController.java
+++ backend/src/main/java/com/springBoot/eBugTracker/controller/CustomerController.java
@@ -7,13 +7,16 @@ import com.springBoot.eBugTracker.entity.bugs.Bug;
 import com.springBoot.eBugTracker.entity.customer.CustomerProfile;
 import com.springBoot.eBugTracker.entity.customer.CustomerProject;
 import com.springBoot.eBugTracker.service.CustomerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/customer/")
 public class CustomerController {
@@ -25,56 +28,116 @@ public class CustomerController {
 
     @PostMapping({"/createCustomerProfile"})
     public CustomerProfileDTO createCustomerProfile(@RequestBody CustomerProfile customerProfile) {
-        return customerService.createCustomerProfile(customerProfile);
+        log.info("Inside;- /CustomerController/createCustomerProfile");
+        try {
+            return customerService.createCustomerProfile(customerProfile);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new CustomerProfileDTO();
     }
 
     @PreAuthorize("hasRole('Customer')")
     @PostMapping({"/createCustomerProject"})
     public CustomerProjectDTO createCustomerProject(@RequestBody CustomerProject customerProject) {
-        return customerService.createCustomerProject(customerProject);
+        log.info("Inside:- /CustomerController/createCustomerProject");
+        try {
+            return customerService.createCustomerProject(customerProject);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new CustomerProjectDTO();
     }
 
     @GetMapping({"/getCustomerProfile/{user_id}"})
     public CustomerProfileDTO getCustomerProfile(@PathVariable String user_id) {
-        return customerService.getCustomerProfile(user_id);
+        log.info("Inside /CustomerController/getCustomerProfile");
+        try {
+            return customerService.getCustomerProfile(user_id);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new CustomerProfileDTO();
     }
 
     @GetMapping({"/getCustomerProfileById/{customer_id}"})
     public CustomerProfileDTO getCustomerProfileById(@PathVariable int customer_id) {
-        return customerService.getCustomerProfileById(customer_id);
+        log.info("Inside:- /CustomerController/getCustomerProfileById");
+        try {
+            return customerService.getCustomerProfileById(customer_id);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new CustomerProfileDTO();
     }
 
     @GetMapping({"/haveCustomerProfile/{username}"})
     public boolean haveCustomerProfile(@PathVariable String username) {
-        return customerService.haveCustomerProfile(username);
+        log.info("Inside:- /CustomerController/haveCustomerProfile");
+        try {
+            return customerService.haveCustomerProfile(username);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return  false;
     }
 
     @PreAuthorize("hasAnyRole('Customer', 'Admin')")
     @GetMapping({"/getCustomerProjects/{profile_id}"})
     public List<CustomerProjectDTO> getCustomerProjects(@PathVariable int profile_id) {
-        return customerService.getCustomerProjects(profile_id);
+        log.info("Inside:- /CustomerController/getCustomerProjects");
+        try {
+            return customerService.getCustomerProjects(profile_id);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new ArrayList<>();
     }
 
     @PreAuthorize("hasRole('Admin')")
     @GetMapping({"/getAllCustomer"})
     public List<CustomerProfileDTO> getAllCustomer() {
-        return customerService.getAllCustomer();
+        log.info("Inside:- /CustomerController/getAllCustomer");
+        try {
+            return customerService.getAllCustomer();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new ArrayList<>();
     }
 
     // Bugs
     @GetMapping({"/getBugs/{project_id}"})
     public List<BugDTO> getBugs(@PathVariable int project_id) {
-        return customerService.getBugs(project_id);
+        log.info("Inside:- /CustomerController/getBugs");
+        try {
+            return customerService.getBugs(project_id);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new ArrayList<>();
     }
 
     @PreAuthorize("hasRole('Customer')")
     @PostMapping({"/addBug"})
     public BugDTO addBug(@RequestBody Bug bug) {
-        return customerService.addBug(bug);
+        log.info("Inside:- /CustomerController/addBug");
+        try {
+            return customerService.addBug(bug);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new BugDTO();
     }
 
     @GetMapping({"/verifyEmail/{email}/{token}"})
     public RedirectView verifyEmail(@PathVariable String email, @PathVariable int token) {
-        return customerService.verifyEmail(email, token);
+        log.info("Inside:- /CustomerController/verifyEmail");
+        try {
+            return customerService.verifyEmail(email, token);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new RedirectView();
     }
 }
